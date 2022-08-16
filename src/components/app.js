6ebf206176fea4b9e8477aaeb136610d64ed77f3
Kundan Singh <kundasin591998@outlook.com>
2022-08-16 14:46:41 +0530

    final project

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import "../App.css";
+import React, { useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import Home from "../components/home/Home";
+import Login from "./login";
+import SignUp from "./signUp";
+import { Route, Redirect, Switch } from "react-router-dom";
+//import AppBar from "./AppBar";
+//import AddCourseForm from "./AddVehicleForm";
+import "react-toastify/dist/ReactToastify.css";
+//import MyCourses from "./MyCourses";
+//import ProtectedRoute from "./common/ProtectedRoute";
+//import AddBooking from "./AddBookings";
 
-const App = () => {
+
+
+function App(props) {
   return (
-    <div>app</div>
-  )
+    <>
+
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+        <Redirect to="/login" />
+      </Switch>
+    </>
+  );
 }
 
-export default App
+export default App;
